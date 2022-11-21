b764944fc315a382c8588ffbd4130922c581b63d
Gaston Artigas <gastonartcia@gmail.com>
2022-11-21 19:14:51 -0300

    arreglé random color < 6 y modifique tamaños

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 document.getElementById("buttom").addEventListener("click", function(){
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    if (randomColor.length < 6) randomColor + "0";
     const color = "#" + randomColor;
     document.querySelector(":root").style.setProperty("--color", color);
     document.getElementById("back").innerText="background-color : " + color;
