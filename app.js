cdf41080ebebb590898a76ea8799071a8ccae308
Gaston Artigas <gastonartcia@gmail.com>
2022-11-21 19:22:19 -0300

    Funcionaba con errores el anterior commit

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 document.getElementById("buttom").addEventListener("click", function(){
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    let i = 0;
-    while(randomColor.length< 6){
-        randomColor + randomColor[i];
-        i ++;
-    }
     const color = "#" + randomColor;
     document.querySelector(":root").style.setProperty("--color", color);
     document.getElementById("back").innerText="background-color : " + color;
