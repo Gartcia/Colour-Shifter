1647b5a4f04c9720560d29d0f981560341618066
Gaston Artigas <gastonartcia@gmail.com>
2022-11-21 19:18:06 -0300

    Refixed length of randomcolor

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 document.getElementById("buttom").addEventListener("click", function(){
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    if (randomColor.length < 6) randomColor + "0";
+    let i = 0;
+    while(randomColor.length< 6){
+        randomColor + randomColor[i];
+        i ++;
+    }
     const color = "#" + randomColor;
     document.querySelector(":root").style.setProperty("--color", color);
     document.getElementById("back").innerText="background-color : " + color;
